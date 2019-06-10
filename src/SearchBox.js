c34f0954b097269b79aff02badbf166be5a176ed
Aditya <adityashnkr5@gmail.com>
2019-06-10 18:02:52 +0530

    Basic styles applied

---
+++ src/SearchBox.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class SearchBox extends Component {
+	render() {
+		return <input className="search-field" type="text" name="textInput" />;
+	}
+}
+
+export default SearchBox;
