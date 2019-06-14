40c1b1f3574946de4a77b5a17536d610000b5f95
Aditya <adityashnkr5@gmail.com>
2019-06-14 12:22:29 +0530

    Updates

--- precache-manifest.1dc40268143d743745dcb009df20446b.js
+++ precache-manifest.1dc40268143d743745dcb009df20446b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6c0ec6ae078d5c4f34fbb4c19fe4f7f7",
+    "revision": "d4db3e2e99eaf4c774ce344799d893b8",
     "url": "/search-api/index.html"
   },
   {
-    "revision": "a1e03f7db36b1a176136",
+    "revision": "435b495a5e2c4a382c6d",
     "url": "/search-api/static/css/main.e59c240d.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/search-api/static/js/2.d6a534e5.chunk.js"
   },
   {
-    "revision": "a1e03f7db36b1a176136",
-    "url": "/search-api/static/js/main.84cea54c.chunk.js"
+    "revision": "435b495a5e2c4a382c6d",
+    "url": "/search-api/static/js/main.9aca04f6.chunk.js"
   },
   {
     "revision": "79594e746d687e7f67d3",
