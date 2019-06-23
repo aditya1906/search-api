30ba10a184b67b11a34bdbd841d7cda1d3416cf9
Aditya <adityashnkr5@gmail.com>
2019-06-23 16:55:46 +0530

    Partially fix fade animation

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import "./App.css";
 import SearchBox from "./components/SearchBox";
 import CardList from "./components/CardList";
 import Loading from "./components/Loading";
-import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
 			fetchUsers: [],
-			users: []
+			users: [],
+			animate: true
 		};
 	}
 	componentDidMount() {
@@ -44,15 +44,7 @@ class App extends Component {
 				<h1>Awesome Search API</h1>
 				<SearchBox onSearchChange={this.onSearchChange} />
 				{this.state.fetchUsers.length !== 0 ? (
-					<ReactCSSTransitionGroup
-						transitionName="fade"
-						transitionAppear={true}
-						transitionAppearTimeout={1000}
-						transitionEnter={false}
-						transitionLeave={false}
-					>
-						<CardList users={this.state.users} />
-					</ReactCSSTransitionGroup>
+					<CardList users={this.state.users} />
 				) : (
 					<Loading />
 				)}
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Card from "./Card";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
 class CardList extends Component {
 	render() {
@@ -7,12 +8,20 @@ class CardList extends Component {
 		const CardArray = users.map((user, i) => {
 			const name = `${users[i].name.first} ${users[i].name.last}`;
 			return (
-				<Card
-					key={i}
-					name={name}
-					email={users[i].email}
-					imgSrc={users[i].picture.large}
-				/>
+				<ReactCSSTransitionGroup
+					transitionName="fade"
+					transitionAppear={true}
+					transitionAppearTimeout={1000}
+					transitionEnter={true}
+					transitionLeave={true}
+				>
+					<Card
+						key={i}
+						name={name}
+						email={users[i].email}
+						imgSrc={users[i].picture.large}
+					/>
+				</ReactCSSTransitionGroup>
 			);
 		});
 		return <div className="card-group">{CardArray}</div>;
