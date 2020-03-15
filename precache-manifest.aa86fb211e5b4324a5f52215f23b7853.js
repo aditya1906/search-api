1561dde7e093406e54481822ebaf002eb8f401ef
root <adityashnkr5@gmail.com>
2020-03-15 23:41:38 +0530

    Updates

---
+++ precache-manifest.aa86fb211e5b4324a5f52215f23b7853.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b8a8442b664930dda427daef554c8c",
+    "url": "/search-api/index.html"
+  },
+  {
+    "revision": "4baa61b6a94f2c614994",
+    "url": "/search-api/static/css/main.f6991ad6.chunk.css"
+  },
+  {
+    "revision": "f809081b285653041b9b",
+    "url": "/search-api/static/js/2.435c69a4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/search-api/static/js/2.435c69a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4baa61b6a94f2c614994",
+    "url": "/search-api/static/js/main.6b744818.chunk.js"
+  },
+  {
+    "revision": "2b0c4f5c08da7c45f9a8",
+    "url": "/search-api/static/js/runtime-main.e0269b02.js"
+  }
+]);
